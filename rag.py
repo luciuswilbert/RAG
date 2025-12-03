a5ae7ce151b22bd1dcf430aec50768b8a072f3c7
luciuswilbert <luciuswilbert@gmail.com>
2025-12-03 17:18:28 +0800

    Automatic RAG

--- rag.py
+++ rag.py
@@ -1,4 +1,6 @@
 import ollama
+import fitz  
+import re
 
 # # Generate the embedding
 # response = ollama.embeddings(
@@ -12,16 +14,31 @@ import ollama
 # print(f"Vector length: {len(vector)}")
 # print(vector[:5]) # Print first 5 dimensions
 
-import fitz  # PyMuPDF
 
-# Open the PDF
-doc = fitz.open("Internship Report Part 2 - Lucius Wilbert Tjoa - TP072404.pdf") 
-full_text = ""
-for page in doc:
-    full_text += page.get_text()
+# # Open the PDF
+# doc = fitz.open("Internship Report Part 2 - Lucius Wilbert Tjoa - TP072404.pdf") 
+# full_text = ""
+# for page in doc:
+#     full_text += page.get_text()
 
-# The 'Clean' Count (Removing all spaces and newlines)
-clean_text = full_text.replace("\n", "").replace("\r", "").replace("\t", "")
+# # The 'Clean' Count (Removing all spaces and newlines)
+# # clean_text = full_text.replace("\n", "").replace("\r", "").replace("\t", "")
+# clean_text = re.sub(r'\s+', ' ', full_text)
 
-with open("Database/report.txt", 'w', encoding='utf-8') as w:
-    w.write(full_text)
+# with open("Database/report.txt", 'w', encoding='utf-8') as w:
+#     w.write(clean_text)
+  
+def read_pdf(filepath):
+  doc = fitz.open(filepath) 
+  full_text = ""
+  for page in doc:
+      full_text += page.get_text()
+  return full_text
+
+def clean_text(full_text):
+   clean_texts = re.sub(r'\s+', ' ', full_text)
+   return clean_texts
+
+def write_text(database_path, clean_texts):
+   with open(database_path, 'w', encoding='utf-8') as x:
+      x.write(clean_texts)
