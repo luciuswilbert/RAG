9c4ef2735a65906708008f7e7cfc119cd2e9598f
luciuswilbert <luciuswilbert@gmail.com>
2025-12-03 09:58:40 +0800

    Numpu

--- rag_scratchpad.py
+++ rag_scratchpad.py
@@ -1,1 +1,9 @@
 import numpy as np
+
+sentence_A = np.array([0.1, 0.5, 0.9])
+sentence_B = np.array([0.2, 0.5, 0.8])
+
+dot_product = np.dot(sentence_A, sentence_B)
+print(dot_product)
+mul = (0.1 * 0.2) + (0.5 * 0.5) + (0.9 * 0.8)
+print(mul)
